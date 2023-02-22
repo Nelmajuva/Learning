1ad022b02d654f56544f0e328505eec168d679d0
Juan Camilo López Morales <jclopezm@garantiascomunitarias.com>
2023-02-22 17:59:18 -0500

    Creada la función encargada de calcular el salario de los empleados mensual

--- PrincipioSOLID/Program.cs
+++ PrincipioSOLID/Program.cs
@@ -2,14 +2,48 @@
 
 class PrincipioSOLID
 {
-    public static void Main(string[] args)
+    public void Main(string[] args)
     {
-        ExportHelper exportHelper = new();
-        StudentRepository studentRepository = new();
-        
-        IEnumerable<Student> listOfStudents = studentRepository.GetAll();
-        exportHelper.ExportStudents(listOfStudents);
+        //Single Child Responsability
+        //ExportHelper exportHelper = new();
+        //StudentRepository studentRepository = new();
 
-        Console.WriteLine("¡Datos de los estudiantes exportados!");
+        //IEnumerable<Student> listOfStudents = studentRepository.GetAll();
+        //exportHelper.ExportStudents(listOfStudents);
+
+        //Console.WriteLine("¡Datos de los estudiantes exportados!");
+    }
+
+    private void CalculateSalaryMonthlyEmployees(List<object> listOfEmployees)
+    {
+        listOfEmployees.ForEach((employee) =>
+        {
+            decimal salary;
+            decimal hourValue;
+
+            if (employee is EmployeeFullTime)
+            {
+                hourValue = 300000M;
+                EmployeeFullTime employeeFullTime = (EmployeeFullTime)employee;
+                salary = hourValue * employeeFullTime.HoursWorked;
+
+                Console.WriteLine($"Empleado {employeeFullTime.FullName}, gana en tiempo completo un total de {salary}");
+
+                return;
+            }
+
+            hourValue = 20000M;
+            EmployeePartialTime employeePartialTime = (EmployeePartialTime)employee;
+            salary = hourValue * employeePartialTime.HoursWorked;
+
+            if (employeePartialTime.HoursWorked > 160)
+            {
+                decimal effortCompensation = 500M;
+                int extraDays = employeePartialTime.HoursWorked - 160;
+                salary += effortCompensation * extraDays;
+            }
+
+            Console.WriteLine($"Empleado {employeePartialTime.FullName}, gana en medio tiempo un total de {salary}");
+        });
     }
 }
